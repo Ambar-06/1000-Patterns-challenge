51259d7719be63e4834a58e70672c58908adefed
Ambar <brannstrom9911@gmail.com>
2022-12-11 15:41:15 +0530

    Day 7

---
+++ pattern019.py
@@ -0,0 +1,13 @@
+# 1 0 1 0 1
+# 0 1 0 1 0
+# 1 0 1 0 1
+# 0 1 0 1 0
+# 1 0 1 0 1
+
+for i in range(1, 6):
+    for j in range(i, i+5):
+        if j%2 != 0:
+            print(1, end=" ")
+        else:
+            print(0, end=" ")
+    print()
