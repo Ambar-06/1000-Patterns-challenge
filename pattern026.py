d19b47ea83ef50c9b3b2c283369b64ab3d31f586
Ambar <brannstrom9911@gmail.com>
2022-12-17 20:31:24 +0530

    Day 8

---
+++ pattern026.py
@@ -0,0 +1,10 @@
+# A A A A A
+# B B B B B
+# C C C C C
+# D D D D D
+# E E E E E
+
+for x in 'ABCDE':
+    for i in range(5):
+        print(x, end=" ")
+    print()
---
+++ pattern027.py
@@ -0,0 +1,10 @@
+# A B C D E
+# A B C D E
+# A B C D E
+# A B C D E
+# A B C D E
+
+for i in range(5):
+    for x in 'ABCDE':
+        print(x, end=" ")
+    print()
